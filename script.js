0603a53c3da506808a8c4196699a0d51ce85e2b0
Vinay Kumar Goud <144577819+vinay1723@users.noreply.github.com>
2026-03-08 22:29:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function mapLetters(word) {
+  let result = {};
+
+  for (let i = 0; i < word.length; i++) {
+    let char = word[i];
+
+    if (!result[char]) {
+      result[char] = [];
+    }
+
+    result[char].push(i);
+  }
+
+  return result;
+}
+
+window.exports = mapLetters;
